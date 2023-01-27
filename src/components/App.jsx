f74e7f20b338eaa02b9e561016ec0ea684a7f2e1
maxyma-sanx <maxymasan.dev@gmail.com>
2023-01-27 11:06:18 +0200

    create default styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import { GlobalStyle } from './GlobalStyle/GlobalStyle';
+import { Layout } from './GlobalStyle/Layout/Layout.styled';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+    <Layout>
       React homework template
-    </div>
+      <GlobalStyle />
+    </Layout>
   );
 };
